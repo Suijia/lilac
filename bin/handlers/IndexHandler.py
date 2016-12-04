08f0db5a4ab6ad5da9227d51440b71fcadf30281
puddingAI <yangsuijia@gmail.com>
2016-12-04 23:23:34 +0800

    [WIP] Issue #1 modify the index page

--- bin/handlers/IndexHandler.py
+++ bin/handlers/IndexHandler.py
@@ -2,16 +2,29 @@
 # -*- coding: utf-8 -*-
 import tornado.log
 from handlers.BaseHandler import BaseHandler
+from crawler.ArticleDao import ArticleDao
 
 __author__ = 'pudding'
 
 
 class IndexHandler(BaseHandler):
+
+    TOTAL_ARTICLE = 3
+    MAX_TITLE = 25
+    MAX_SNIPPET = 87
+
     def initialize(self, conf):
         super(IndexHandler, self).initialize(conf)
 
     def get(self):
-        title_list = ["I am pudding", "I am snowwolf"]
-        self.render_with_user('index.html', title_list=title_list)
+        article_dao = ArticleDao()
+        articles = article_dao.get_latest_articles(IndexHandler.TOTAL_ARTICLE)
+        for article in articles:
+            if len(article.title) >= IndexHandler.MAX_TITLE:
+                article.cover = article.cover[0: IndexHandler.MAX_TITLE] + "..."
+            if len(article.snippet) >= IndexHandler.MAX_SNIPPET:
+                article.snippet = article.snippet[0: IndexHandler.MAX_SNIPPET] + "..."
+
+        self.render_with_user('index.html', articles=articles)
 
     post = get
